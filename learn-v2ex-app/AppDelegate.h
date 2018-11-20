35bed65478a9fd24c21bd637902a9e03043dd43d
Smile <xcxerxes@aliyun.com>
2018-11-20 21:28:59 +0800

    Initial Commit

---
+++ learn-v2ex-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learn-v2ex-app
+//
+//  Created by xiacan on 2018/11/20.
+//  Copyright © 2018 iotek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learn-v2ex-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learn-v2ex-app
+//
+//  Created by xiacan on 2018/11/20.
+//  Copyright © 2018 iotek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
